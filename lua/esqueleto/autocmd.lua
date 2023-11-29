b0f9032d7cef43fa5e2b1aa68dcb25fc0df10512
z775729168 <z775729168@gmail.com>
2023-11-29 10:46:50 +0800

    fix: check empty filepath

--- lua/esqueleto/autocmd.lua
+++ lua/esqueleto/autocmd.lua
@@ -1,28 +1,26 @@
-local utils = require('esqueleto.utils')
+local utils = require("esqueleto.utils")
 
 local M = {}
 
 M.createautocmd = function(opts)
-  -- create autocommands for skeleton insertion
-  local group = vim.api.nvim_create_augroup(
-    "esqueleto",
-    { clear = true }
-  )
+	-- create autocommands for skeleton insertion
+	local group = vim.api.nvim_create_augroup("esqueleto", { clear = true })
 
-  vim.api.nvim_create_autocmd(
-    { "BufWinEnter", "BufReadPost", "FileType" },
-    {
-      group = group,
-      desc = "esqueleto.nvim :: Insert template",
-      pattern = opts.patterns,
-      callback = function()
-        local filepath = vim.fn.expand("%")
-        local emptyfile = vim.fn.getfsize(filepath) < 4
-        if emptyfile then utils.inserttemplate(opts) end
-      end
-    }
-  )
+	vim.api.nvim_create_autocmd({ "BufWinEnter", "BufReadPost", "FileType" }, {
+		group = group,
+		desc = "esqueleto.nvim :: Insert template",
+		pattern = opts.patterns,
+		callback = function()
+			local filepath = vim.fn.expand("%")
+			if filepath == "" then
+				return
+			end
+			local emptyfile = vim.fn.getfsize(filepath) < 4
+			if emptyfile then
+				utils.inserttemplate(opts)
+			end
+		end,
+	})
 end
-
 
 return M
